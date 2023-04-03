31bd4c5cf95502c2934fd9c5bba19b922f8d2a33
longjiang <l502858283@gmail.com>
2023-04-03 18:33:19 +0800

    修改basePath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const removeImports = require("next-remove-imports")();
 const debug = process.env.NODE_ENV !== "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: !debug ? '/mingjianglong.github.io/' : '',
+  basePath: !debug ? '/mingjianglong.github.io' : '',
   assetPrefix: !debug ? '/mingjianglong.github.io/' : '',
   images: {
     // default, imgix, cloudinary, akamai
