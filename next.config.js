3c4dd2b172ca28b960656c5258f4dee362b6abaa
longjiang <l502858283@gmail.com>
2023-04-03 15:21:28 +0800

    修改assetPrefix路径

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const removeImports = require("next-remove-imports")();
 const debug = process.env.NODE_ENV !== "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // assetPrefix: debug ? "" : ".",
+  assetPrefix: debug ? "" : ".",
   images: {
     // default, imgix, cloudinary, akamai
     loader: 'imgix',
