a811f4a3b641992bf71d3a3f531d59ec3e0e1dc7
longjiang <l502858283@gmail.com>
2023-04-03 17:00:32 +0800

    增加action 修改静态资源路径

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const removeImports = require("next-remove-imports")();
 const debug = process.env.NODE_ENV !== "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: debug ? "" : ".",
+  basePath: !debug ? '/mingjianglong.github.io/' : '',
+  assetPrefix: !debug ? '/mingjianglong.github.io/' : '',
   images: {
     // default, imgix, cloudinary, akamai
     loader: 'imgix',
@@ -13,16 +14,6 @@ const nextConfig = {
   cleanDistDir: true,
   // output: "export",
   reactStrictMode: true,
-  //Specified "redirects" cannot but used with "output: export"
-  // redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/blog",
-  //       permanent: true
-  //     }
-  //   ]
-  // },
   eslint: {
     ignoreDuringBuilds: true
   },
