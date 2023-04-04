1e235d408dfcc3025931b1f0bc9e83e8aaf04e94
longjiang <l502858283@gmail.com>
2023-04-04 17:33:12 +0800

    手动打包

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const removeImports = require("next-remove-imports")();
 const debug = process.env.NODE_ENV !== "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: !debug ? '/mingjianglong.github.io' : '',
-  // assetPrefix: !debug ? '/mingjianglong.github.io/' : '',
+  basePath: !debug ? '/mingjianglong.github.io' : '',
+  assetPrefix: !debug ? '/mingjianglong.github.io/' : '',
   images: {
     // default, imgix, cloudinary, akamai
     loader: 'imgix',
