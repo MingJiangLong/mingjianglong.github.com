6ca9170ca1185b5f5e7d4b0f1a3a94d53abf7466
longjiang <l502858283@gmail.com>
2023-04-19 18:25:51 +0800

    babel配置

--- next.config.js
+++ next.config.js
@@ -11,29 +11,5 @@ const nextConfig = {
   basePath: !debug ? '/mingjianglong.github.io' : '',
   assetPrefix: !debug ? '/mingjianglong.github.io' : '',
 }
-// const nextConfig = {
-//   basePath: !debug ? '/mingjianglong.github.io' : '',
-//   assetPrefix: !debug ? '/mingjianglong.github.io/' : '',
-//   images: {
-//     // default, imgix, cloudinary, akamai
-//     loader: 'imgix',
-//     path: '/',
-//   },
-//   // distDir: "docs",
-//   cleanDistDir: true,
-//   // output: "export",
-//   reactStrictMode: true,
-//   eslint: {
-//     ignoreDuringBuilds: true
-//   },
-//   typescript: {
-//     // !! WARN !!
-//     // Dangerously allow production builds to successfully complete even if
-//     // your project has type errors.
-//     // !! WARN !!
-//     ignoreBuildErrors: true,
-//   },
-
-// }
 
 module.exports = removeImports(nextConfig)
