55a9027c3334bf46e728ec30e649d8664aad2635
mingjianglong <l502858283@gmail.com>
2024-05-24 14:25:23 +0800

    feat: 提交

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -10,13 +10,6 @@ export default function Avatar(props: AvatarProps) {
     <div className={styles["container"]}>
       <div className={styles["image-container"]}>
         <img src="/images/avatar.png" alt="mingjianglong" className={styles["avatar"]} />
-        {/* <Image
-          src="/avatar.png"
-          alt="avatar"
-          width="100"
-          height="100"
-          className={styles["avatar"]}
-        /> */}
         <h2>龙江</h2>
         <h3 style={{ fontWeight: "normal", paddingTop: ".6em", color: "grey" }}>
           Jiang Long
