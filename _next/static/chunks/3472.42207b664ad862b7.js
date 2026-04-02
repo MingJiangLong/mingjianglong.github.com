11f9d93a372e751f04742897cb8dc54da2021db3
MingJiangLong <92349393+MingJiangLong@users.noreply.github.com>
2026-04-02 06:48:08 +0000

    Deploying to lj-pages from  @ 5423f8be69dcd29656484f834cb23c72c32972bf 🚀

--- _next/static/chunks/3472.42207b664ad862b7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3472],{93472:function(e,r,c){c.r(r),c.d(r,{ArchitectureModule:function(){return t.s},createArchitectureServices:function(){return t.i}});var t=c(21775);c(9423)}}]);
---
+++ _next/static/chunks/7035.8014f2f764e25452.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7035],{67035:function(e,r,c){c.r(r),c.d(r,{ArchitectureModule:function(){return t.s},createArchitectureServices:function(){return t.i}});var t=c(25345);c(96218)}}]);
